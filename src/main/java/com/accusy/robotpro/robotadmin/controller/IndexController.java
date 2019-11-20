03b8ba47eb16a3168d470cbd88a5bdd81473b9b1
Audra Zapata <audra.zapata@delsur.com.ve>
2019-11-20 11:40:15 -0400

    titulo: cambios relacionados con el servicios christian gutierrez

--- src/main/java/com/accusy/robotpro/robotadmin/controller/IndexController.java
+++ src/main/java/com/accusy/robotpro/robotadmin/controller/IndexController.java
@@ -1,22 +1,33 @@
 package com.accusy.robotpro.robotadmin.controller;
 
-
 import com.accusy.robotpro.robotadmin.model.UsuarioIO;
 import com.accusy.robotpro.robotadmin.services.ServicesRobot;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@PropertySource("classpath:application.properties")
 public class IndexController {
 
     @Autowired
     ServicesRobot ser;
+    @Value("${spring.name.administrator.global}")
+    private String administradorGobal;
+    @Value("${spring.users.administrator.global}")
+    private String administradorUserGobal;
 
-//    @RequestMapping(value = "/", method = RequestMethod.GET)
+//    @RequestMapping(value spring.datasource.diver= "/", method = RequestMethod.GET)
 //    public String printWelcome(ModelMap model) {
 //        String url;
 //
@@ -31,23 +42,35 @@ public class IndexController {
 //
 //        return url;
 //    }
-    
-    
-
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public ModelAndView login(HttpSession session) {
+        boolean flag = false;
         ModelAndView model;
         String usuario = "";
         usuario = System.getProperty("user.name");
-        UsuarioIO user = ser.getUsuarioByLogin(usuario);
-        if (user != null) {
-            model = new ModelAndView("login");
-        } else {
-            session.setAttribute("UsuarioSession", user);
-            model = new ModelAndView("main/fichaUnicaDatos");
-        }
 
+        if (administradorGobal.equals(usuario)) {
+            flag = true;
+            UsuarioIO user = ser.getUsuarioByLogin(administradorUserGobal);
+            if (user != null) {
+                model = new ModelAndView("main/fichaUnicaDatos");
+                session.setAttribute("UsuarioAdmin", user);
+            } else {
+                model = new ModelAndView("login");
+            }
+
+        } else {
+            UsuarioIO user = ser.getUsuarioByLogin(usuario);
+            if (user != null) {
+                session.setAttribute("UsuarioSession", user);
+                model = new ModelAndView("main/fichaUnicaDatos");
+            } else {
+                model = new ModelAndView("login");
+            }
+        }
         model.addObject("paso", 2);
+        model.addObject("admin", flag);
+
         return model;
     }
 }
