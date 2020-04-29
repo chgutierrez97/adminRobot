d622e5cc18ffa5fd60836a75ecbca4f137d3b7d0
Audra Zapata <audra.zapata@delsur.com.ve>
2020-04-29 10:10:49 -0400

    Creado por:Christian Gutierrez Descripcion:modificacion de propiedades de las tablas Fecha:28042020

--- src/main/webapp/resources/js/admin_transaccion.js
+++ src/main/webapp/resources/js/admin_transaccion.js
@@ -1,8 +1,39 @@
 $(document).ready(function () {
-    $('#dataTable2').DataTable();
-    $('#tableUsuarios').DataTable();
-    $('#tableTransacciones').DataTable();
-    $('#tableExpresiones').DataTable();
+    $('#tableUsuarios, #tableTransacciones,#dataTable2, #tableExpresiones').DataTable({
+        
+        "order": [[ 1, "asc" ]], 
+        "oLanguage": {
+            "oPaginate": {
+                "sPrevious": "Anterior",
+                "sNext": "Siguiente",
+                "sLast": "&Uacute;ltima",
+                "sFirst": "Primera"
+            },
+
+            "sLengthMenu": 'Mostrar <select>' +
+                    '<option value="5">5</option>' +
+                    '<option value="10">10</option>' +
+                    '<option value="20">20</option>' +
+                    '<option value="30">30</option>' +
+                    '<option value="40">40</option>' +
+                    '<option value="50">50</option>' +
+                    '</select> registros',
+
+            "sInfo": "Mostrando del _START_ a _END_ (Total: _TOTAL_ resultados)",
+
+            "sInfoFiltered": " - filtrados de _MAX_ registros",
+
+            "sInfoEmpty": "No hay resultados de b&uacute;squeda",
+
+            "sZeroRecords": "No hay registros a mostrar",
+
+            "sProcessing": "Espere, por favor...",
+
+            "sSearch": "Buscar:"       
+            
+        }
+    });
+
     
     
 //    $("#numImputs").keyup(function () {
